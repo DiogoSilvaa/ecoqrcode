ce7de4ce27a508560f5d447463b553e00d153129
DiogoSilvaa <44171970+DiogoSilvaa@users.noreply.github.com>
2020-05-21 01:07:58 +0100

    Changes to app file

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.length('/', function(req, res){
+    res.render('index',{});
+})
 app.use(express.static(__dirname+'/public'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
