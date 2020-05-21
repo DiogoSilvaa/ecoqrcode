c5a09a9e46e0d3ea586d8d92baff5a642c7db151
DiogoSilvaa <44171970+DiogoSilvaa@users.noreply.github.com>
2020-05-21 01:10:06 +0100

    Update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.length('/', function(req, res){
+app.use(express.static(__dirname+'/public'));
+app.get('/', function(req, res){
     res.render('index',{});
 })
-app.use(express.static(__dirname+'/public'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log('Our app is running on port ${Port}');
