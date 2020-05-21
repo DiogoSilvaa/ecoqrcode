4c4c44b3cb1805122b057711bd41e92cfc382e75
DiogoSilvaa <44171970+DiogoSilvaa@users.noreply.github.com>
2020-05-21 01:27:20 +0100

    update

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const http = require('http');
 
 let app = http.createServer((req,res)=>{
     res.render('public/index',{});
+    res.end();
 }) 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>{
-    console.log('Our app is running on port ${Port}');
-});
